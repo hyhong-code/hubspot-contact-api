6d7c9bfc2f54662bdcf7547b60c698a1bf66c334
Haiyang Hong <hong961127@gmail.com>
2020-11-23 16:38:52 -0800

    Add update pug view

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
+const path = require("path");
 const axios = require("axios");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/contacts", async (req, res, next) => {
   try {
@@ -16,6 +22,21 @@ app.get("/contacts", async (req, res, next) => {
   }
 });
 
-const port = 3000;
+app.get("/update", async (req, res, next) => {
+  try {
+    const { email } = req.query;
 
-app.listen(port, () => console.log(`Server up on port ${port}...`));
+    const { data } = await axios.get(
+      `https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HUBSPOT_API_KEY}`
+    );
+
+    console.log(email, data.properties.favorite_book.value);
+
+    res.render("update", { userEmail: email, favoriteBook: data.properties.favorite_book.value });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`🚀 Server up on port ${port}... `));
