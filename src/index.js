1563556ab4fa1e760d017384c09e01b13eeb767b
Haiyang Hong <hong961127@gmail.com>
2020-11-23 16:47:58 -0800

    Add update favorite book post route

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// List all contacts
 app.get("/contacts", async (req, res, next) => {
   try {
     const contacts = `https://api.hubapi.com/contacts/v1/lists/all/contacts/recent?hapikey=${process.env.HUBSPOT_API_KEY}`;
@@ -22,6 +23,7 @@ app.get("/contacts", async (req, res, next) => {
   }
 });
 
+// Update favorite books view
 app.get("/update", async (req, res, next) => {
   try {
     const { email } = req.query;
@@ -30,9 +32,28 @@ app.get("/update", async (req, res, next) => {
       `https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HUBSPOT_API_KEY}`
     );
 
-    console.log(email, data.properties.favorite_book.value);
-
     res.render("update", { userEmail: email, favoriteBook: data.properties.favorite_book.value });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Update favorite book
+app.post("/update", async (req, res, next) => {
+  try {
+    const { newVal } = req.body;
+    const { email } = req.query;
+
+    const body = {
+      properties: [{ property: "favorite_book", value: newVal }],
+    };
+
+    await axios.post(
+      `https://api.hubapi.com/contacts/v1/contact/email/${email}/profile?hapikey=${process.env.HUBSPOT_API_KEY}`,
+      body
+    );
+
+    res.redirect("back");
   } catch (error) {
     console.error(error);
   }
